5a84818ce8e9196b96157e61c2e61c28ed76ed01
Nukufel <vogeln@bzz.ch>
2022-05-11 11:22:11 +0200

    service

--- src/main/java/ch/bzz/bookshelf/model/Book.java
+++ src/main/java/ch/bzz/bookshelf/model/Book.java
@@ -9,4 +9,52 @@ public class Book {
     private Publisher publisher;
     private BigDecimal price;
     private String isbn;
+
+    public String getBookUUID() {
+        return bookUUID;
+    }
+
+    public void setBookUUID(String bookUUID) {
+        this.bookUUID = bookUUID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
