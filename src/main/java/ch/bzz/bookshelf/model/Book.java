6cc0f7996cf8ae3a1428989b9d298e17482ebb02
Nukufel <vogeln@bzz.ch>
2022-05-11 10:31:52 +0200

    inital commit

---
+++ src/main/java/ch/bzz/bookshelf/model/Book.java
@@ -0,0 +1,12 @@
+package ch.bzz.bookshelf.model;
+
+import java.math.BigDecimal;
+
+public class Book {
+    private String bookUUID;
+    private String title;
+    private String author;
+    private Publisher publisher;
+    private BigDecimal price;
+    private String isbn;
+}
